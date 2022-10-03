fd82ba8193aed150b0d81b7dd5dfeab76a0bed4d
a857296 <noureddine.karri@atos.net>
2022-10-03 13:57:42 +0200

    create app

---
+++ src/main/java/com/api/bolough/BoloughApplication.java
@@ -0,0 +1,13 @@
+package com.api.bolough;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoloughApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoloughApplication.class, args);
+	}
+
+}
