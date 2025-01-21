24f7d40c459a1161ac26a9d8b512121c18d21774
NOUREDDINE KARRI <noureddine.karri@atos.net>
2025-01-21 15:53:26 +0100

    FRONT: add mat-toolbar

--- bolough-ui/src/app/shared/shared.module.ts
+++ bolough-ui/src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { FrContentComponent } from './content/fr-content/fr-content.component';
 import { EnContentComponent } from './content/en-content/en-content.component';
 import { ArContentComponent } from './content/ar-content/ar-content.component';
+import { MaterialComponent } from './material/material.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { ArContentComponent } from './content/ar-content/ar-content.component';
   exports:[
     FrContentComponent,
     EnContentComponent,
-    ArContentComponent
+    ArContentComponent,
+    MaterialComponent
   ]
 })
 export class SharedModule { }
